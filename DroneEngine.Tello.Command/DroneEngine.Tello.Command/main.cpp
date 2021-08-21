87bbcd01ffe4b324b8dd331ab7a7b1ebb28193ff
alir14 <ali_r14@yahoo.com>
2021-08-22 08:38:51 +1200

    add rc commands

--- DroneEngine.Tello.Command/DroneEngine.Tello.Command/main.cpp
+++ DroneEngine.Tello.Command/DroneEngine.Tello.Command/main.cpp
@@ -30,27 +30,35 @@ int main()
 				break;
 			case 'w':
 				std::cout << "forward" << std::endl;
-				tello.SendCommand("forward 45");
+				tello.SendCommand("rc 0 20 0 0");
 				break;
 			case 's':
 				std::cout << "backward" << std::endl;
-				tello.SendCommand("back 45");
+				tello.SendCommand("rc 0 -20 0 0");
 				break;
 			case 'a':
 				std::cout << "left" << std::endl;
-				tello.SendCommand("left 45");
+				tello.SendCommand("rc -20 0 0 0");
 				break;
 			case 'd':
 				std::cout << "right" << std::endl;
-				tello.SendCommand("right 45");
+				tello.SendCommand("rc 20 0 0 0");
+				break;
+			case 'c':
+				std::cout << "right" << std::endl;
+				tello.SendCommand("rc 0 0 0 20");
+				break;
+			case 'x':
+				std::cout << "right" << std::endl;
+				tello.SendCommand("rc 0 0 0 -20");
 				break;
 			case 'q':
 				std::cout << "turn left" << std::endl;
-				//tello.SendCommand("back 45");
+				tello.SendCommand("ccw 30");
 				break;
 			case 'e':
 				std::cout << "turn right" << std::endl;
-				//tello.SendCommand("back 45");
+				tello.SendCommand("cw 15");
 				break;
 			default:
 				std::cout << "unknown command: " << inputChar << std::endl;
